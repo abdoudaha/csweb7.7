cc54177d499dbfbe8f466261157b8b6aac5e448a
abdoudaha <abderrahmen.rafai@gmail.com>
2025-05-27 19:29:04 +0100

    first commit

---
+++ src/AppBundle/config.php
@@ -0,0 +1,13 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBUSER', 'bahi');
+define('DBPASS', 'bahi2020@');
+define('DBNAME', 'enquetes_db');
+define('ENABLE_OAUTH', true);
+define('FILES_FOLDER', 'C:\wamp64\www\csweb7.7\files');
+define('DEFAULT_TIMEZONE', 'UTC');
+define('MAX_EXECUTION_TIME', '300');
+define('API_URL', 'http://localhost/csweb7.7/api/');
+define('CSWEB_LOG_LEVEL' , 'error');
+define('CSWEB_PROCESS_CASES_LOG_LEVEL', 'error');
+?>
